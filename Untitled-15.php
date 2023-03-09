2005535cf4d286f622e3e25c0e08f927ed9ca25f
nioo-NT <@tristanio_armanto_ts5_22@student.smktelkom-sda.sch.id>
2023-03-09 10:17:17 +0700

    Untitled-15.php

--- Untitled-15.php
+++ Untitled-15.php
@@ -14,6 +14,21 @@
                 <th>stok barang</th>
                 <th>harga</th>
             </tr>
+            <?php
+            include "Untitled-14.php";
+            $query_mysql =mysqli_query($koneksi,"SELECT * FROM barang")or die(mysqli_error());
+            $nomor = 1;
+            while($data = mysqli_fetch_array($query_mysql)){
+          ?>
+            <tr>
+                <td><?php echo $data ['id_barang']; ?></td>
+                <td><?php echo $data ['nama_barang']; ?></td>
+                <td><?php echo $data ['tanggal_terima']; ?></td>
+                <td><?php echo $data ['stok_barang']; ?></td>
+                <td><?php echo $data ['harga']; ?></td>
+            </tr>
+            <?php } ?>
+
         </table>
         
     </body>
